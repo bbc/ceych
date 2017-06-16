0398c8f3af74a2760a1b13548b3cfd76c6a704e8
nick spragg <nick.spragg@bbc.co.uk>
2017-06-16 13:06:10 +0100

    Refactor: Make tests promise based

--- test/lib/memoize.js
+++ test/lib/memoize.js
@@ -9,7 +9,11 @@ const hash = require('../../lib/hash');
 const memoize = require('../../lib/memoize');
 const packageVersion = require('../../package.json').version;
 
-const wrappable = (cb) => cb(null, 1);
+const wrappableWithCb = (cb) => cb(null, 1);
+const wrappable = () => {
+  return Promise.resolve(1);
+};
+
 const opts = {
   ttl: 30,
   suffix: ''
@@ -36,64 +40,64 @@ describe('memoize', () => {
   });
 
   describe('hashing', () => {
-    it('hashes the function as a string when there are no arguments', (done) => {
+    it('hashes the function as a string when there are no arguments', () => {
       const func = memoize(cacheClient, opts, wrappable);
 
-      func((err) => {
-        assert.ifError(err);
-        sinon.assert.calledWith(hash.create, `${wrappable.toString()}[]`);
-        done();
-      });
+      func()
+        .catch(assert.ifError)
+        .then(() => {
+          sinon.assert.calledWith(hash.create, `${wrappable.toString()}[]`);
+        });
     });
 
-    it('hashes the function as a string along with its arguments', (done) => {
-      const wrappableMultipleArgs = (one, two, three, four, cb) => {
-        return cb(null, 1);
+    it('hashes the function as a string along with its arguments', () => {
+      const wrappableMultipleArgs = (one, two, three, four) => {
+        return Promise.resolve(four - three - two - one + three);
       };
       const func = memoize(cacheClient, opts, wrappableMultipleArgs);
 
-      func(1, 2, 3, 4, (err) => {
-        assert.ifError(err);
-        sinon.assert.calledWith(hash.create, `${wrappableMultipleArgs.toString()}[1,2,3,4]`);
-        done();
-      });
+      return func(1, 2, 3, 4)
+        .catch(assert.ifError)
+        .then(() => {
+          sinon.assert.calledWith(hash.create, `${wrappableMultipleArgs.toString()}[1,2,3,4]`);
+        });
     });
 
-    it('hashes the function as a string along with its arguments and a suffix', (done) => {
+    it('hashes the function as a string along with its arguments and a suffix', () => {
       const differentOpts = _.cloneDeep(opts);
       differentOpts.suffix = 'some differentiating suffix';
 
-      const wrappableMultipleArgs = (one, two, three, four, cb) => {
-        return cb(null, 1);
+      const wrappableMultipleArgs = (one, two, three, four) => {
+        return Promise.resolve(four - three - two - one + three);
       };
       const func = memoize(cacheClient, differentOpts, wrappableMultipleArgs);
 
-      func(1, 2, 3, 4, (err) => {
-        assert.ifError(err);
-        sinon.assert.calledWith(hash.create, `${wrappableMultipleArgs.toString()}[1,2,3,4]some differentiating suffix`);
-        done();
-      });
+      func(1, 2, 3, 4)
+        .catch(assert.ifError)
+        .then(() => {
+          sinon.assert.calledWith(hash.create, `${wrappableMultipleArgs.toString()}[1,2,3,4]${differentOpts.suffix}`);
+        });
     });
 
-    it('stringifies objects before hashing', (done) => {
-      const wrappableWithObject = (obj, cb) => {
-        return cb(null, obj);
+    it('stringifies objects before hashing', () => {
+      const wrappableWithObject = (obj) => {
+        return Promise.resolve(obj);
       };
       const func = memoize(cacheClient, opts, wrappableWithObject);
 
-      func({
-        testing: '123'
-      }, (err) => {
-        assert.ifError(err);
-        sinon.assert.calledWith(hash.create, `${wrappableWithObject.toString()}[{"testing":"123"}]`);
-        done();
-      });
+      return func({
+          testing: '123'
+        })
+        .catch(assert.ifError)
+        .then(() => {
+          sinon.assert.calledWith(hash.create, `${wrappableWithObject.toString()}[{"testing":"123"}]`);
+        });
     });
   });
 
   describe('caching', () => {
     describe('retrieving from cache', () => {
-      it('returns the result from the cache if one exists', (done) => {
+      it('returns the result from the cache if one exists', () => {
         const cachedValue = Promise.resolve({
           item: 1
         });
@@ -103,14 +107,14 @@ describe('memoize', () => {
 
         const func = memoize(cacheClient, opts, wrappable);
 
-        func((err, results) => {
-          assert.ifError(err);
-          assert.strictEqual(results, 1);
-          done();
-        });
+        func()
+          .catch(assert.ifError)
+          .then((results) => {
+            assert.strictEqual(results, 1);
+          });
       });
 
-      it('does not call the wrapped function if there is a result in the cache', (done) => {
+      it('does not call the wrapped function if there is a result in the cache', () => {
         cacheClient.getAsync.withArgs(sinon.match({
           id: 'hashed'
         })).returns(Promise.resolve({
@@ -120,14 +124,14 @@ describe('memoize', () => {
         const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.notCalled(wrappableStub);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.notCalled(wrappableStub);
+          });
       });
 
-      it('returns a null value if it exists in the cache', (done) => {
+      it('returns a null value if it exists in the cache', () => {
         cacheClient.getAsync.withArgs(sinon.match({
           id: 'hashed'
         })).returns(Promise.resolve({
@@ -137,90 +141,91 @@ describe('memoize', () => {
         const wrappableStub = sandbox.stub().returns();
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err, results) => {
-          assert.ifError(err);
-          sinon.assert.notCalled(wrappableStub);
-          assert.strictEqual(results, null);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then((results) => {
+            sinon.assert.notCalled(wrappableStub);
+            assert.strictEqual(results, null);
+          });
       });
 
-      it('does not attempt to retrieve from the cache if the cache is not ready', (done) => {
+      it('does not attempt to retrieve from the cache if the cache is not ready', () => {
         cacheClient.isReady.returns(false);
 
         const wrappableStub = sandbox.stub().returns();
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.notCalled(cacheClient.getAsync);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.notCalled(cacheClient.getAsync);
+          });
       });
 
-      it('returns an error to the callback if retrieving from the cache fails', (done) => {
-        const func = memoize(cacheClient, opts, wrappable);
+      it('returns an error to the callback if retrieving from the cache fails', () => {
+        const func = memoize(cacheClient, opts, wrappableWithCb);
         cacheClient.getAsync.returns(Promise.reject(new Error('GET Error!')));
 
-        func((err) => {
-          assert.ok(err);
-          assert.strictEqual(err.message, 'GET Error!');
-          done();
-        });
+        return func()
+          .then(() => {
+            assert.fail('Expected error to be returned!');
+          })
+          .catch((err) => {
+            assert.strictEqual(err.message, 'GET Error!');
+          });
       });
     });
 
     describe('calling wrapped function', () => {
-      it('calls the wrapped function when the cache does not return anything', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, 1);
+      it('calls the wrapped function when the cache does not return anything', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err, results) => {
-          assert.ifError(err);
-          sinon.assert.called(wrappableStub);
-          assert.strictEqual(results, 1);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then((results) => {
+            sinon.assert.called(wrappableStub);
+            assert.strictEqual(results, 1);
+          });
       });
 
-      it('calls the wrapped function with its arguments when the cache does not return anything', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, 1);
+      it('calls the wrapped function with its arguments when the cache does not return anything', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func(1, {
-          two: 'three'
-        }, [4], 'five', (err) => {
-          assert.ifError(err);
-          sinon.assert.calledWith(wrappableStub, 1, sinon.match({
+        return func(1, {
             two: 'three'
-          }), [4], 'five', sinon.match.func);
-          done();
-        });
+          }, [4], 'five')
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.calledWith(wrappableStub, 1, sinon.match({
+              two: 'three'
+            }), [4], 'five');
+          });
       });
 
-      it('supports multiple return values', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, 1, 2, 3);
+      it('supports multiple return values', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve([1, 2, 3]));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err, resultOne, resultTwo, resultThree) => {
-          assert.ifError(err);
-          assert.equal(resultOne, 1);
-          assert.equal(resultTwo, 2);
-          assert.equal(resultThree, 3);
-          done();
-        });
+        func()
+          .catch(assert.ifError)
+          .then((results) => {
+            assert.equal(results[0], 1);
+            assert.equal(results[1], 2);
+            assert.equal(results[2], 3);
+          });
       });
 
-      it('returns an error to the callback if the wrapped function throws an error', (done) => {
-        const wrappableStub = sandbox.stub().yields(new Error('Function Error!'));
+      it('returns an error to the callback if the wrapped function throws an error', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.reject(new Error('Function Error!')));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ok(err);
-          sinon.assert.called(wrappableStub);
-          assert.strictEqual(err.message, 'Function Error!');
-          done();
-        });
+        return func()
+          .catch((err) => {
+            sinon.assert.called(wrappableStub);
+            assert.strictEqual(err.message, 'Function Error!');
+          });
       });
     });
 
@@ -229,108 +234,93 @@ describe('memoize', () => {
         cacheClient.getAsync.returns(Promise.resolve(null, null));
       });
 
-      it('sets the results of the function in the cache', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, 1);
+      it('sets the results of the function in the cache', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
-            id: 'hashed'
-          }), [null, 1]);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
+              id: 'hashed'
+            }), [null, 1]);
+          });
       });
 
-      it('sets a return value of null to the cache', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, null);
+      it('sets a return value of null to the cache', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(null));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
-            id: 'hashed'
-          }), [null, null]);
-          done();
-        });
+        return func()
+          .then(() => {
+            sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
+              id: 'hashed'
+            }), [null, null]);
+          });
       });
 
-      it('sets the TTL as the expiry in milliseconds', (done) => {
+      it('sets the TTL as the expiry in milliseconds', () => {
         const differentOpts = _.cloneDeep(opts);
         differentOpts.ttl = 10;
 
-        const wrappableStub = sandbox.stub().yields(null, 1);
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, differentOpts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
-            id: 'hashed'
-          }), [null, 1], 10000);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
+              id: 'hashed'
+            }), [null, 1], 10000);
+          });
       });
 
-      it('includes the package version in the key object', (done) => {
-        const wrappableStub = sandbox.stub().yields(null, 1);
+      it('includes the package version in the key object', () => {
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
-            segment: `ceych_${packageVersion}`
-          }), [null, 1]);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.calledWith(cacheClient.setAsync, sinon.match({
+              segment: `ceych_${packageVersion}`
+            }), [null, 1]);
+          });
       });
 
-      it('does not attempt to set to the cache if the cache is not ready', (done) => {
+      it('does not attempt to set to the cache if the cache is not ready', () => {
         cacheClient.isReady.returns(false);
 
-        const wrappableStub = sandbox.stub().yields(null, 1);
+        const wrappableStub = sandbox.stub().returns(Promise.resolve(1));
         const func = memoize(cacheClient, opts, wrappableStub);
 
-        func((err) => {
-          assert.ifError(err);
-          sinon.assert.notCalled(cacheClient.setAsync);
-          done();
-        });
+        return func()
+          .catch(assert.ifError)
+          .then(() => {
+            sinon.assert.notCalled(cacheClient.setAsync);
+          });
       });
 
-      it('returns an error to the callback if saving to the cache fails', (done) => {
+      it('returns an error to the callback if saving to the cache fails', () => {
         cacheClient.setAsync.returns(Promise.reject(new Error('SET Error!')));
         const func = memoize(cacheClient, opts, wrappable);
 
-        func((err) => {
-          assert.ok(err);
-          assert.strictEqual(err.message, 'SET Error!');
-          done();
-        });
+        return func()
+          .catch((err) => {
+            assert.strictEqual(err.message, 'SET Error!');
+          });
       });
     });
   });
 
   describe('wrapped function parameters', () => {
-    it('returns an error when one of the arguments cannot be stringified', (done) => {
+    it('returns an error when one of the arguments cannot be stringified', () => {
       const func = memoize(cacheClient, opts, wrappable);
 
-      func(new Circular(), (err) => {
-        assert.ok(err);
-        assert.strictEqual(err.message, 'Failed to create cache key from arguments: Converting circular structure to JSON');
-        done();
-      });
-    });
-
-    it('throws an error if a callback is not passed as an argument', () => {
-      const func = memoize(cacheClient, opts, wrappable);
-      assert.throw(func, Error, 'Wrapped function must be passed a callback');
-    });
-
-    it('throws an error if the final argument is not a function', () => {
-      const func = memoize(cacheClient, opts, wrappable);
-      assert.throw(() => {
-        func(1, 2, 3);
-      }, Error, 'Final argument of wrapped function must be a callback');
+      func(new Circular())
+        .catch((err) => {
+          assert.strictEqual(err.message, 'Failed to create cache key from arguments: Converting circular structure to JSON');
+        });
     });
   });
 
@@ -348,7 +338,7 @@ describe('memoize', () => {
     });
 
     it('increments a StatsD counter every time there is a cache miss', (done) => {
-      const func = memoize(cacheClient, optsWithStats, wrappable);
+      const func = memoize(cacheClient, optsWithStats, wrappableWithCb);
 
       func((err) => {
         assert.ifError(err);
@@ -359,7 +349,7 @@ describe('memoize', () => {
 
     it('increments a StatsD counter every time there is a cache hit', (done) => {
       cacheClient.getAsync.returns(Promise.resolve([null, 1]));
-      const func = memoize(cacheClient, optsWithStats, wrappable);
+      const func = memoize(cacheClient, optsWithStats, wrappableWithCb);
 
       func((err) => {
         assert.ifError(err);
